cec62aa11c5021cba14c0e5ffdff90615536cdf6
丞之 <zmq175@qq.com>
2016-04-26 17:00:20 +0800

    opencvtest

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -1,15 +1,101 @@
-//
-//  main.cpp
-//  opencv
-//
-//  Created by 丞之 on 16/4/15.
-//  Copyright © 2016年 丞之. All rights reserved.
-//
+
+//! [includes]
+
+#include <opencv2/core/core.hpp>
+
+#include <opencv2/imgcodecs.hpp>
+
+#include <opencv2/highgui/highgui.hpp>
+
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+
+//! [includes]
+
+
+//! [namespace]
+
+using namespace cv;
+
+//! [namespace]
+
+
+using namespace std;
+
+
+int main( int argc, char** argv )
+
+{
+    
+    //! [load]
+    
+    string imageName("//Users//user//Documents//1.png"); // by default
+    
+    if( argc > 1)
+        
+    {
+        
+        imageName = argv[1];
+        
+    }
+    
+    //! [load]
+    
+    
+    
+    //! [mat]
+    
+    Mat image;
+    
+    //! [mat]
+    
+    
+    
+    //! [imread]
+    
+    image = imread(imageName.c_str(), IMREAD_COLOR); // Read the file
+    
+    //! [imread]
+    
+    
+    
+    if( image.empty() )                      // Check for invalid input
+        
+    {
+        
+        cout <<  "Could not open or find the image" << std::endl ;
+        
+        return -1;
+        
+    }
+    
+    
+    
+    //! [window]
+    
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
+    
+    //! [window]
+    
+    
+    
+    //! [imshow]
+    
+    imshow( "Display window", image );                // Show our image inside it.
+    
+    //! [imshow]
+    
+    
+    
+    //! [wait]
+    
+    waitKey(0); // Wait for a keystroke in the window
+    
+    //! [wait]
+    
     return 0;
+    
 }
